1e400d942ec18c5304b936b72620af849e90a481
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-08-14 12:01:00 +0530

    Updates

---
+++ precache-manifest.66dbac6aebf97d3bf5071c9cebb8a727.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b365aeda5d5dae9c19db482de790c162",
+    "url": "/CodeCamp_13-Stripe_Submenu/index.html"
+  },
+  {
+    "revision": "e1e5c1da59fb29f4a571",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/css/main.ec0347cb.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e5c1da59fb29f4a571",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/js/main.0a76dc59.chunk.js"
+  },
+  {
+    "revision": "2364bee53cea516d7696",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/js/runtime-main.966f1222.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/CodeCamp_13-Stripe_Submenu/static/media/phone.e908706c.svg"
+  }
+]);
